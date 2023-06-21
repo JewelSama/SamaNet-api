7f710bcd0138c19a2fea100de4728862e33e1bc7
Jewel Osafile <osafilejewel@gmail.com>
2023-06-21 14:21:38 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from "express"
+import dotenv from "dotenv"
+import userRoutes from "./routes/userRoutes"
+
+
+
+
+dotenv.config()
+const app = express()
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+const port = process.env.Port 
+
+app.use('/user', userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to SamaNet');
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+const router = Router()
+
+
+router.post('/login', (req, res) => {
+    res.sendStatus(200);
+})
+
+
+
+
+
+
+export default router;
