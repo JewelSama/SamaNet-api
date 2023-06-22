d438c7fc66b192ca6642d0edef8f4d78318c2192
Jewel Osafile <osafilejewel@gmail.com>
2023-06-22 19:43:24 +0100

    SIB email integration

---
+++ prisma/migrations/20230621205954_token_models/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Token" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "type" TEXT NOT NULL,
+    "emailToken" TEXT,
+    "valid" BOOLEAN NOT NULL DEFAULT true,
+    "expiration" DATETIME NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Token_emailToken_key" ON "Token"("emailToken");
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,123 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs"
+import { createTransport } from "nodemailer";
+import { getImpliedNodeFormatForFile } from "typescript";
+// @ts-ignore
+import SibApi from "sib-api-v3-sdk"
+
+const SibClient = SibApi.ApiClient.instance;
+
+SibClient.authentications['api-key'].apiKey = process.env.SIB_API_KEY
+
+const transactionEmailApi = new SibApi.TransactionalEmailsApi();
+
+let smtpMailData = new SibApi.SendSmtpEmail();
+
+
+const sender = {
+    email: 'osafilejewel@gmail.com', // your email address
+    name: 'SamaNet',
+  };
 
 const router = Router()
+const prisma = new PrismaClient()
 
 
-router.post('/login', (req, res) => {
-    res.sendStatus(200);
+const JWT_SECRET = process.env.JWT_SECRET || "JWT SECRET"
+
+
+
+
+
+
+
+
+// register
+router.post('/register', async(req, res) => {
+    const { username, email, firstname, lastname, password } = req.body
+
+    if(!(username && email && firstname && lastname && password)){
+        return res.status(400).json({ error: "All inputes are required" })
+    }
+    // console.log(req.body)
+    const attemptedUser = await prisma.user.findUnique({ where: { email: email } })  
+    
+    const invalidUsername = await prisma.user.findUnique({ where: { username: username  }}) 
+
+    if(attemptedUser){
+        return res.status(409).json({ error: "User Already Exist. Please Login" })
+    }
+
+    if(invalidUsername){
+        return res.status(409).json({ error: "Username is not available!" })
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10)
+    // console.log(encryptedPassword) 
+  /*  try {
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email: email.toLowerCase(),
+                firstname,
+                lastname,
+                password: encryptedPassword
+            }
+        }) 
+        console.log(JWT_SECRET)
+        res.status(201).json(newUser)
+    } catch (error) {
+        console.log(error)
+    } */
+
+
+    try {
+        smtpMailData.sender = sender;
+
+       smtpMailData.to = [{
+            email: email,
+            name: firstname
+        }];
+
+       smtpMailData.subject = 'You are on the samaNet!';
+
+       smtpMailData.params = {
+            'name': firstname,
+            'twitter': '@samanet'
+        };
+
+       smtpMailData.htmlContent = "<html><body><p>Hello {{ params.name }}, "
+                 + "welcome to SamaNet. We'll notify you "
+                  + "when we launch. Kindly follow us on Twitter "
+                 + "{{ params.twitter }}. arigato!</p></body></html>";
+
+        // send email
+        await transactionEmailApi.sendTransacEmail(smtpMailData)
+        // @ts-ignore
+            .then((data) => { 
+                console.log(data) // log the email id
+            })
+            // @ts-ignore
+            .catch((error) => {
+                console.error(error)
+                throw new Error(error) // handle errors
+            })
+    } catch (error) {
+        console.log('An error occured...')
+        console.error(error)
+        // @ts-ignore
+        throw new Error(error) // handle errors
+    }
+
+
+    
+    
+    
+
+
+
+
 })
 
 
