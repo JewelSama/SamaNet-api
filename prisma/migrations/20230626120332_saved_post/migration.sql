16a2beca1abf51ac92c11d631d43c726c26485e2
Jewel Osafile <osafilejewel@gmail.com>
2023-06-27 16:47:43 +0100

    imgs

--- prisma/migrations/20230626120332_saved_post/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[postId]` on the table `Savedpost` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Savedpost_postId_key" ON "Savedpost"("postId");
---
+++ prisma/migrations/20230627151547_caption_in_comment_model/migration.sql
@@ -0,0 +1,2 @@
+-- DropIndex
+DROP INDEX "Comment_postId_key";
