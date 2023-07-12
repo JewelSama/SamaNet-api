8e851b0d1ae969ef9abe94b8a6926e3559c81da3
Jewel Osafile <67759825+JewelSama@users.noreply.github.com>
2023-07-12 15:50:53 +0100

    Update index.ts static file

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import profileRoutes from "./routes/profileRoutes"
 
 dotenv.config()
 const app = express()
-app.use(express.static('../public'))
+app.use(express.static('public'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
